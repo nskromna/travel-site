17e2c4fda23e878cfa41b7680c78f1141ffc578e
Natalia <nataliaskromna@gmail.com>
2022-09-29 16:53:03 +0200

    completed reveal on scroll

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -1,9 +1,9 @@
 class MobileMenu {
     constructor() {
-        this.menuIcon = document.querySelector('.site-header__menu-icon')
-        this.menuContent = document.querySelector('.site-header__menu-content')
-        this.siteHeader = document.querySelector('.site-header')
-        this.events()
+        this.menuIcon = document.querySelector('.site-header__menu-icon');
+        this.menuContent = document.querySelector('.site-header__menu-content');
+        this.siteHeader = document.querySelector('.site-header');
+        this.events();
         }
 
         events() {
@@ -11,11 +11,11 @@ class MobileMenu {
         }
 
         toggleTheMenu() {
-            this.menuContent.classList.toggle('site-header__menu-content--is-visible')
-            this.siteHeader.classList.toggle('site-header--is-expanded')
-            this.menuIcon.classList.toggle('site-header__menu-icon--close-x')
+            this.menuContent.classList.toggle('site-header__menu-content--is-visible');
+            this.siteHeader.classList.toggle('site-header--is-expanded');
+            this.menuIcon.classList.toggle('site-header__menu-icon--close-x');
         }
     }
 
 
-export default MobileMenu
+export default MobileMenu;
