011e5758448c2c77a2d85b76916915e85a939ec3
Natalia <nataliaskromna@gmail.com>
2022-10-10 16:09:04 +0200

    updated cloud function to check password

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,6 +1,26 @@
 exports.handler = (event, contex, callback) => {
-    callback(null, {
-        statusCode: 200,
-        body: 'Welcome to the super secret area'
-    });
+    const secretContent = `
+    <h3>Welcome to The Secret Area</h3>
+    <p>Here we can tell you is <strong>blue</strong>, and two plus two equals four.</p>
+    `;
+    let body;
+
+    if (event.body) {
+        body = JSON.parse(event.body);
+    } else {
+        body = {};
+    }
+
+    if (body.password == 'javascript') {
+        callback(null, {
+            statusCode: 200,
+            body: secretContent
+        });
+    } else {
+        callback(null, {
+            statusCode: 401
+        });
+
+    }
+
 };
